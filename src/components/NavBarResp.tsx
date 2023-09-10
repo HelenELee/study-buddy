270e5dfa276695a372981e07ae7e3ea3c1bebe90
HelenELee <HelenELee3@outlook.com>
2023-09-10 12:29:06 +0800

    Added "another" link in copy

--- src/components/NavBarResp.tsx
+++ src/components/NavBarResp.tsx
@@ -55,6 +55,9 @@ const NavBarResp = () => {
          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
            Updated New Link
          </a>
+         <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
+           Another link in copy
+         </a>
        </div>
        {/* <div>
          <button className="inline-flex items-center bg-amber-500 border-0 py-2 px-4 text-white">
