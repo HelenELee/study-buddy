754739553959075eabb81d8a926719002f14f52e
HelenELee <HelenELee3@outlook.com>
2023-09-10 09:36:36 +0800

    Test commit process in IDE

--- src/App copy.tsx
+++ src/App copy.tsx
@@ -3,10 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import NavBar from './components/NavBar';
-import NavBarResp from './components/NavBarResp';
-
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 // Construct our main GraphQL API endpoint
@@ -37,8 +34,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
-      <NavBarResp />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          HELLO! Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
       <h1 className="text-3xl font-bold underline text-red-600">
       Simple React Typescript Tailwind Sample
     </h1>
